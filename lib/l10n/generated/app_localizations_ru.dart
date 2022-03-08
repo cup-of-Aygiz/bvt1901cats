a34ccb22d5fb3f1767c9cc99ee66f1cd261b50e5
aygiz <factum8181@yandex.ru>
2022-03-08 22:05:07 +0300

    1

---
+++ lib/l10n/generated/app_localizations_ru.dart
@@ -0,0 +1,18 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get aqua => 'Вода';
+
+  @override
+  String get noInternet => '';
+
+  @override
+  String get commonError => '';
+}
