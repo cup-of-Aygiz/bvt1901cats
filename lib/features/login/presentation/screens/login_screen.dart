279e76bec187de39159a41027df000525a881d5b
Tatyana108 <zaqwsx2222222222@gmail.com>
2022-04-06 12:06:35 +0300

    feature/AQUA-27_add_detector

--- lib/features/login/presentation/screens/login_screen.dart
+++ lib/features/login/presentation/screens/login_screen.dart
@@ -2,13 +2,13 @@ import 'package:bvt1901_practice/app/presentation/theme/app_text_style.dart';
 import 'package:bvt1901_practice/features/registration/presentation/screen/registration_screen.dart';
 import 'package:bvt1901_practice/uikit/app_bars/default_app_bar.dart';
 import 'package:bvt1901_practice/uikit/buttons/app_text_button.dart';
-import 'package:bvt1901_practice/uikit/buttons/no_background-button.dart';
 import 'package:bvt1901_practice/uikit/text_fields/app_phone_field.dart';
 import 'package:bvt1901_practice/uikit/text_fields/app_text_field.dart';
 import 'package:bvt1901_practice/uikit/validators/app_validators.dart';
 import 'package:bvt1901_practice/utils/extentions/app_context.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../uikit/buttons/app_button_detector.dart';
 import '../../../registration/presentation/components/proggres_gradient.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -21,7 +21,6 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-
     final locale = context.appLocale;
     final colors = context.appColors;
     final router = context.appRouter;
@@ -40,7 +39,7 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             Text(
               locale.project_name,
-              style: AppTextStyle.normalW200S34H24,
+              style: AppTextStyle.normalW200S34,
               textAlign: TextAlign.center,
             ),
             Padding(
@@ -69,14 +68,14 @@ class _LoginScreenState extends State<LoginScreen> {
               children: [
                 Text(
                   locale.no_registration_yet,
-                  style: AppTextStyle.normalW400S12H24,
+                  style: AppTextStyle.normalW400S12,
                 ),
-                NoBackgroundButton(
-                  buttonText: locale.create_account,
+                AppGestureDetector(
                   onPressed: () {
                     router.pushScreen(context, const RegistrationScreen());
                   },
-                  style: AppTextStyle.normalW700S12H24,
+                  style: AppTextStyle.normalW700S12,
+                  text: locale.create_account,
                 )
               ],
             ),
