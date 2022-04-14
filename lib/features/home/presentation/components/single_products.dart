1bccbf7befdd8dce95783880ccdb58503ea1f8a6
Dennn <64165146+Den-7@users.noreply.github.com>
2022-04-14 16:36:10 +0300

    AQUA-26_Danil_home_screen

--- lib/features/home/presentation/components/single_products.dart
+++ lib/features/home/presentation/components/single_products.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: non_constant_identifier_names, prefer_typing_uninitialized_variables
+// ignore_for_file: non_constant_identifier_names, prefer_typing_uninitialized_variables, use_key_in_widget_constructors
 
 import 'package:bvt1901_practice/utils/extentions/app_context.dart';
 import 'package:flutter/material.dart';
