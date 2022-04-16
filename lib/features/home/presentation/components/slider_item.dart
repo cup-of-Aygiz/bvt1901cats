8e5ac631f4d6e2be8f6f6ef8561f7efcc2a90d91
Tatyana108 <zaqwsx2222222222@gmail.com>
2022-04-16 21:13:29 +0300

    добавлен набросок слайдера с акциями

---
+++ lib/features/home/presentation/components/slider_item.dart
@@ -0,0 +1,38 @@
+import 'package:bvt1901_practice/utils/extentions/app_context.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SliderItem extends StatelessWidget {
+  final String text;
+
+  const SliderItem({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.appColors;
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(horizontal: 6.w),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(16.r),
+        border: Border.all(
+          width: 2.w,
+          color: colors.darkOcean,
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 50.h),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/screen/home_screen.dart
+++ lib/features/home/presentation/screen/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:bvt1901_practice/features/home/presentation/components/slider_item.dart';
 import 'package:bvt1901_practice/uikit/app_bars/default_app_bar.dart';
 import 'package:bvt1901_practice/utils/extentions/app_context.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../products_catalog/presetation/components/products_list.dart';
 import '../../../registration/presentation/components/proggres_gradient.dart';
@@ -11,10 +14,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colors = context.appColors;
+    final locale = context.appLocale;
     return Scaffold(
-      /// TODO поменять
-      appBar: const DefaultAppBar(
-        titleText: '',
+      appBar: DefaultAppBar(
+        titleText: locale.project_name,
       ),
       body: BackgroundProgressWidget(
           colorOne: colors.darkOcean,
@@ -23,7 +26,22 @@ class HomeScreen extends StatelessWidget {
             top: false,
             child: Stack(
               children: [
-                const Expanded(child: ProductList()),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 12.h, vertical: 10.h),
+                  child: CarouselSlider(
+                    options: CarouselOptions(height: 130.h),
+                    items: [
+                      SliderItem(text: locale.free_delivery),
+                      SliderItem(text: locale.free_delivery),
+                      SliderItem(text: locale.free_delivery),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 150.h),
+                  child: const Expanded(child: ProductList()),
+                ),
                 BackgroundProgressWidget(
                   length: 20,
                   colorThree: colors.ocean,
