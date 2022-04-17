77443365c217c9c4879aa638259301f8fb531815
Tatyana108 <zaqwsx2222222222@gmail.com>
2022-04-18 01:32:58 +0300

    добавлена кнопка для добавления в избранное

--- lib/features/products_catalog/presetation/components/product_container.dart
+++ lib/features/products_catalog/presetation/components/product_container.dart
@@ -1,13 +1,15 @@
 import 'package:bvt1901_practice/app/presentation/theme/app_text_style.dart';
 import 'package:bvt1901_practice/utils/extentions/app_context.dart';
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:like_button/like_button.dart';
 import '../../domain/entity/product_entity.dart';
 
 class ProductContainer extends StatelessWidget {
   final ProductEntity productEntity;
+
   const ProductContainer({
     Key? key,
     required this.productEntity,
@@ -22,30 +24,52 @@ class ProductContainer extends StatelessWidget {
       child: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           return Container(
-            margin: EdgeInsets.symmetric(horizontal: 4.w, vertical: 8.h),
+            margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
             padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ClipRRect(
-                  borderRadius: BorderRadius.circular(30.r),
-                  child: FancyShimmerImage(
-                    imageUrl: productEntity.image,
-                    width: constraints.maxWidth,
-                    height: constraints.maxWidth,
-                    boxFit: BoxFit.fill,
-                    boxDecoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30.r),
-                    ),
+                  borderRadius: BorderRadius.circular(25.r),
+                  child: Stack(
+                    children: [
+                      FancyShimmerImage(
+                        imageUrl: productEntity.image,
+                        width: constraints.maxWidth,
+                        height: constraints.maxWidth,
+                        boxFit: BoxFit.fill,
+                        boxDecoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.r),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 116.w, top: 6.h),
+                        width: 32.h,
+                        height: 32.h,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: myColors.white,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 2.w,top: 2.w),
+                          child: const LikeButton(),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 4.w),
-                  child: Text(
-                    productEntity.name,
-                    style: AppTextStyle.normalW700S18,
-                    textAlign: TextAlign.left,
-                    maxLines: 2,
+                  child: Row(
+                    children: [
+                      Text(
+                        productEntity.name,
+                        style: AppTextStyle.normalW700S12,
+                        textAlign: TextAlign.left,
+                        maxLines: 2,
+                      ),
+                      SizedBox(width: 10.h),
+                    ],
                   ),
                 ),
                 SizedBox(height: 4.h),
@@ -53,7 +77,7 @@ class ProductContainer extends StatelessWidget {
                   padding: EdgeInsets.symmetric(horizontal: 4.w),
                   child: Text(
                     '${productEntity.price} ₽',
-                    style: AppTextStyle.normalW700S16
+                    style: AppTextStyle.normalW700S12
                         .copyWith(color: myColors.black),
                   ),
                 ),
