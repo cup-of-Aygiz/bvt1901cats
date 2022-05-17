3d9910eb39af6ebda54339ad085d56e775dcad3c
aygiz <factum8181@yandex.ru>
2022-05-17 13:35:09 +0300

    выход из аккаутна

--- lib/features/profile/presentation/screen/profile_screen.dart
+++ lib/features/profile/presentation/screen/profile_screen.dart
@@ -1,13 +1,15 @@
 import 'package:bvt1901_practice/features/adreses_page/presentation/screens/addresses_page.dart';
+import 'package:bvt1901_practice/features/login/domain/state/login_cubit.dart';
 import 'package:bvt1901_practice/features/person_data/presentation/screens/person_data_screen.dart';
+import 'package:bvt1901_practice/features/welcom_page/presentation/screens/welcom_page_screen.dart';
 import 'package:bvt1901_practice/utils/extentions/app_context.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../app/presentation/theme/app_text_style.dart';
 import '../../../../gen/assets.gen.dart';
 import '../../../../uikit/app_bars/default_app_bar.dart';
-import '../../../login/presentation/screens/login_screen.dart';
 import '../../../orders/presentation/screens/order_screen.dart';
 import '../components/profile_button.dart';
 
@@ -62,9 +64,14 @@ class ProfileScreen extends StatelessWidget {
           ProfileButton(
             icon: Assets.icons.icLeave.svg(width: 30.h, height: 30.h),
             text: locale.leave_account,
-            onTap: () {
-              router.pushScreen(context, const LoginScreen(),
-                  rootNavigator: true);
+            onTap: () async {
+              await context
+                  .read<LoginCubit>()
+                  .logOut()
+                  .then((value) => router.pushAndRemoveAllBefore(
+                        context,
+                        const WelcomePageScreen(),
+                      ));
             },
           ),
         ],
