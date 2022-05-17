e0bb3ea708e1189bb1a9cb60da9516dd0f48403b
Tatyana108 <zaqwsx2222222222@gmail.com>
2022-05-17 22:06:34 +0300

    исправление с токеном

--- lib/features/basket/data/network_basket_repository.dart
+++ lib/features/basket/data/network_basket_repository.dart
@@ -1,5 +1,4 @@
 import 'package:bvt1901_practice/app/data/network_servise/dio_container.dart';
-import 'package:bvt1901_practice/app/data/network_servise/token_interceptor.dart';
 import 'package:bvt1901_practice/features/basket/domain/basket_repository.dart';
 import 'package:bvt1901_practice/features/product_page/data/dto/product_details_dto.dart';
 import 'package:bvt1901_practice/features/product_page/domain/entity/product_details_entity.dart';
@@ -7,6 +6,8 @@ import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../../app/data/exceptions/newtwork_error_mapper.dart';
+import '../../../di/service_locator.dart';
+import '../../login/data/auth_secure_storage.dart';
 
 @Injectable(as: BasketRepository)
 class BasketNetworkRepository extends BasketRepository {
@@ -18,15 +19,19 @@ class BasketNetworkRepository extends BasketRepository {
   Future<List<ProductDetailsEntity>> addInProductList(
       {required int productId, required int amount}) async {
     try {
+      final String? token = await getIt<AuthTokenStorage>().loadAccessToken();
       final response = await dioContainer.dio.post('/cart',
           queryParameters: {
             'id': productId,
             'amount': amount,
           },
-          options: Options(extra: {AuthTokenInterceptor.tag: true}));
+          options: Options(headers: {
+            'x-access-token': token,
+          }),
+      );
 
-      return List<ProductDetailsEntity>.from(
-          response.data.map((e) => ProductDetailsDTO.fromJson(e).toDetailsEntity()));
+      return List<ProductDetailsEntity>.from(response.data
+          .map((e) => ProductDetailsDTO.fromJson(e).toDetailsEntity()));
     } catch (e) {
       throw mapToErrorModel(e);
     }
@@ -36,16 +41,19 @@ class BasketNetworkRepository extends BasketRepository {
   Future<List<ProductDetailsEntity>> deleteFromProductList(
       {required int productId}) async {
     try {
+      final String? token = await getIt<AuthTokenStorage>().loadAccessToken();
       final response = await dioContainer.dio.delete(
         '/cart',
         queryParameters: {
           'id': productId,
         },
-        options: Options(extra: {AuthTokenInterceptor.tag: true}),
+        options: Options(headers: {
+          'x-access-token': token,
+        }),
       );
 
-      return List<ProductDetailsEntity>.from(
-          response.data.map((e) => ProductDetailsDTO.fromJson(e).toDetailsEntity()));
+      return List<ProductDetailsEntity>.from(response.data
+          .map((e) => ProductDetailsDTO.fromJson(e).toDetailsEntity()));
     } catch (e) {
       throw mapToErrorModel(e);
     }
@@ -54,13 +62,16 @@ class BasketNetworkRepository extends BasketRepository {
   @override
   Future<List<ProductDetailsEntity>> getProductList() async {
     try {
+      final String? token = await getIt<AuthTokenStorage>().loadAccessToken();
       final response = await dioContainer.dio.get(
         '/cart',
-        options: Options(extra: {AuthTokenInterceptor.tag: true}),
+          options: Options(headers: {
+            'x-access-token': token,
+          }),
       );
 
-      return List<ProductDetailsEntity>.from(
-          response.data.map((e) => ProductDetailsDTO.fromJson(e).toDetailsEntity()));
+      return List<ProductDetailsEntity>.from(response.data
+          .map((e) => ProductDetailsDTO.fromJson(e).toDetailsEntity()));
     } catch (e) {
       throw mapToErrorModel(e);
     }
@@ -70,17 +81,20 @@ class BasketNetworkRepository extends BasketRepository {
   Future<List<ProductDetailsEntity>> updateProductList(
       {required int productId, required int amount}) async {
     try {
+      final String? token = await getIt<AuthTokenStorage>().loadAccessToken();
       final response = await dioContainer.dio.put(
         '/cart',
         queryParameters: {
           'productId': productId,
           'amount': amount,
         },
-        options: Options(extra: {AuthTokenInterceptor.tag: true}),
+        options: Options(headers: {
+          'x-access-token': token,
+        }),
       );
 
-      return List<ProductDetailsEntity>.from(
-          response.data.map((e) => ProductDetailsDTO.fromJson(e).toDetailsEntity()));
+      return List<ProductDetailsEntity>.from(response.data
+          .map((e) => ProductDetailsDTO.fromJson(e).toDetailsEntity()));
     } catch (e) {
       throw mapToErrorModel(e);
     }
